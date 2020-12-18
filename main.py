2c9395a6b41140ee32c147f1319890e9e4b3c1ea
sesamechee <wuhengchee@gmail.com>
2020-12-18 14:56:31 +0800

    add fb scraper

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+from configparser import ConfigParser
+from scraper import CollectPosts
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Non API public FB miner')
+
+  parser.add_argument('-p', '--pages', nargs='+',
+                      dest="pages",
+                      help="List the pages you want to scrape for recent posts")
+
+  parser.add_argument("-d", "--depth", action="store",
+                      dest="depth", default=10, type=int,
+                      help="How many recent posts you want to gather -- in multiples of (roughly) 8.")
+
+  args = parser.parse_args()
+
+proDir = os.path.split(os.path.realpath(__file__))[0]
+configPath = os.path.join(proDir, "config.ini")
+
+cfg = ConfigParser()
+cfg.read(configPath)
+
+if not args.pages:
+    print("Something went wrong!")
+    print(parser.print_help())
+    exit()
+
+if args.pages:
+    C = CollectPosts(ids=args.pages, depth=args.depth)
+    C.connectDB(cfg['db']['host'], cfg['db']['user'], cfg['db']['passwd'], cfg['db']['database'])
+    C.login(cfg['facebook']['email'], cfg['facebook']['password'])
+    C.collect("pages")
---
+++ scraper.py
@@ -0,0 +1,276 @@
+import os
+import time
+import sys
+import re
+import mysql.connector
+from bs4 import BeautifulSoup
+from datetime import datetime
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+class CollectPosts(object):
+
+  def __init__(self, ids, depth=10, delay=2):
+
+    self.ids = ids
+    self.depth = depth
+    self.delay = delay
+    self.current = 0
+    
+    option = Options()
+    option.add_argument("--disable-notifications")
+    self.browser = webdriver.Chrome(chrome_options=option)
+
+  def safe_find_element_by_id(self, elem_id):
+    try:
+        return self.browser.find_element_by_id(elem_id)
+    except NoSuchElementException:
+        return None
+
+  def safe_find_element_by_xpath(self, path):
+    try:
+        return self.browser.find_element_by_xpath(path)
+    except NoSuchElementException:
+        return None
+
+  def connectDB(self, host, user, passwd, database):
+    self.db = mysql.connector.connect(
+        host=host,
+        user=user,
+        passwd=passwd,
+        database=database
+    )
+    self.dbcursor = self.db.cursor()
+    print(self.db)
+
+  def collect(self, typ):
+    if typ == "pages":
+      for iden in self.ids:
+        self.collect_page(iden)
+    self.browser.close()
+
+  def login(self, email, password):
+    try:
+      self.browser.get("https://www.facebook.com")
+      # self.browser.maximize_window()
+
+      # filling the form
+      self.browser.find_element_by_name('email').send_keys(email)
+      self.browser.find_element_by_name('pass').send_keys(password)
+
+      # clicking on login button
+      self.browser.find_element_by_id('u_0_b').click()
+      # if your account uses multi factor authentication
+      mfa_code_input = self.safe_find_element_by_id('approvals_code')
+
+      if mfa_code_input is None:
+          return
+
+      mfa_code_input.send_keys(input("Enter MFA code: "))
+      self.browser.find_element_by_id('checkpointSubmitButton').click()
+
+      # there are so many screens asking you to verify things. Just skip them all
+      while self.safe_find_element_by_id('checkpointSubmitButton') is not None:
+        dont_save_browser_radio = self.safe_find_element_by_id('u_0_3')
+        if dont_save_browser_radio is not None:
+            dont_save_browser_radio.click()
+
+        self.browser.find_element_by_id('checkpointSubmitButton').click()
+
+    except Exception:
+        print("There was some error while logging in.")
+        print(sys.exc_info()[0])
+        exit()
+
+  def get_soup (self):
+    return BeautifulSoup(self.browser.page_source, "lxml")
+
+  def collect_page(self, page):
+    self.browser.get('https://www.facebook.com/' + page + '/')
+
+    time.sleep(self.delay)
+
+    while self.current < self.depth :
+      htmltext = self.browser.page_source
+      soup = BeautifulSoup(htmltext, "lxml")
+
+      postList = soup.find_all('div', 'du4w35lb k4urcfbm l9j0dhe7 sjgh65i0')
+      postTotal = len(postList)
+      print('postTotal: ' + str(postTotal) + ' | current: ' + str(self.current))
+
+      for post in postList[self.current:] : # Start from current postcount
+          print('current Post: ' + str(self.current))
+          # Extract id of each Post
+          postLabelList = post.find('div', {'class': 'lzcic4wl', 'role': 'article'})['aria-describedby'].split(' ')
+
+          # Get post id + link + type + date
+          if self.safe_find_element_by_id(postLabelList[0]) is not None :
+            postLinkElem = self.safe_find_element_by_xpath('//*[@id="'+ postLabelList[0] +'"]//a')
+            if postLinkElem is not None :
+              self.browser.execute_script('arguments[0].scrollIntoView({ block: "center" })', postLinkElem)
+              ActionChains(self.browser).move_to_element(postLinkElem).perform()
+              time.sleep(self.delay)
+            soup = self.get_soup()
+            link = postLinkElem.get_attribute('href').split("?")[0]
+            post_id = self.extract_post_id(link)
+            typename = self.extract_post_type(link)
+            post_created_date = soup.find('span', {'role': 'tooltip'}).text # Orginal format = 2020年12月08日星期三18:00
+            post_created_date = re.sub(r"星期+(一|二|三|四|五|六|日)", "", post_created_date) # cut 星期三
+            post_created_date = datetime.strptime(post_created_date, '%Y年%m月%d日%H:%M')
+          
+          # Get post caption
+          if self.safe_find_element_by_id(postLabelList[1]) is not None :
+            readmoreElem = self.safe_find_element_by_xpath('//*[@id="' + postLabelList[1] + '"]//*[text()="查看更多"]')
+            if readmoreElem is not None:
+              self.browser.execute_script('arguments[0].scrollIntoView({ block: "center" })', readmoreElem)
+              ActionChains(self.browser).move_to_element(readmoreElem).click().perform()
+            caption = self.browser.find_element_by_id(postLabelList[1]).text
+            
+          # Get post comments
+          comment_count = 0
+          share_count = 0
+          postTool = post.find('div', 'bp9cbjyn j83agx80 pfnyh3mw p1ueia1e').find_all('div', {'role': 'button'})
+          for item in postTool :
+            if '回應' in item.text :
+              comment_count = re.findall(r"\d+", item.text.replace(',', ''))[0]
+            elif '分享' in item.text:
+              share_count = re.findall(r"\d+", item.text.replace(',', ''))[0]
+
+
+          # Get post emoji
+          if self.safe_find_element_by_id(postLabelList[3]) is not None :
+            emojiTypeList = {
+              'tc5IAx58Ipa': { 'label': 'like', 'val': 0 },
+              'MB1XWOdQjV0': { 'label': 'heart', 'val': 0 },
+              'bkP6GqAFgZ_': { 'label': 'haha', 'val': 0 },
+              'QTVmPoFjk5O': { 'label': 'hug', 'val': 0 },
+              'PByJ079GWfl': { 'label': 'angry', 'val': 0 },
+              'tHO3j6Ngeyx': { 'label': 'wow', 'val': 0 },
+              '1eqxxZX7fYp': { 'label': 'sad', 'val': 0 }
+            }
+
+            # Open Emoji Popup
+            emojiToolElem = self.safe_find_element_by_xpath('//*[@id="'+ postLabelList[3] +'"]/span')
+            if emojiToolElem is not None :
+              self.browser.execute_script('arguments[0].scrollIntoView({ block: "center" })', emojiToolElem)
+              ActionChains(self.browser).move_to_element(emojiToolElem).click().perform()
+              time.sleep(self.delay)
+              soup = self.get_soup()
+              emojiPopup = soup.find('div', {'role': 'dialog', 'aria-label': '心情數量'})
+              emojiList = emojiPopup.find('div', 'soycq5t1 l9j0dhe7').find_all('div', {'role': 'tab'})
+              for emojiItem in emojiList[2:] : # Ignore first 2 tabs
+                emojiSrcName = emojiItem.find('img')['src'].split('/')[-1].split('.')[0]
+                emojiTypeList[emojiSrcName]['val'] = int(emojiItem.text.replace(',', ''))
+
+              # Close Emoji Popup
+              emojiClose = self.browser.find_element_by_xpath('//div[@role="button"][@aria-label="關閉"]')
+              ActionChains(self.browser).move_to_element(emojiClose).click().perform()
+
+          postObj = {
+              'user_id': page,
+              'post_id': post_id,
+              'typename': typename,
+              'like_count': emojiTypeList['tc5IAx58Ipa']['val'],
+              'heart_count': emojiTypeList['MB1XWOdQjV0']['val'],
+              'haha_count': emojiTypeList['bkP6GqAFgZ_']['val'],
+              'hug_count': emojiTypeList['QTVmPoFjk5O']['val'],
+              'angry_count': emojiTypeList['PByJ079GWfl']['val'],
+              'wow_count': emojiTypeList['tHO3j6Ngeyx']['val'],
+              'sad_count': emojiTypeList['1eqxxZX7fYp']['val'],
+              'comment_count': comment_count,
+              'share_count': share_count,
+              'caption': caption,
+              'link': link,
+              'post_created_date': post_created_date.strftime('%Y-%m-%d %H:%M:%S'),
+              'last_updated_date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+          }
+
+          self.insert_db_post(postObj)
+          self.current = self.current + 1
+
+    #Scroll
+    self.browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    time.sleep(self.delay)
+
+  def extract_post_id (self, link) :
+    return list(filter(None, link.split('/')))[-1]
+
+  def extract_post_type (self, link) :
+    if '/videos/' in link :
+      return 'videos'
+    elif '/photos/' in link:
+      return 'photos'
+    else :
+      return 'posts'
+
+  def insert_db_post (self, postObj) :
+    # Check Duplicate Post by post_id
+    self.dbcursor.execute("SELECT * FROM facebook_posts WHERE post_id = '"+ postObj['post_id'] +"'")
+    sqlpost = self.dbcursor.fetchall()
+
+    if len(sqlpost) > 0 :
+      # Update Post
+      sql = """UPDATE facebook_posts SET
+                like_count = %s,
+                heart_count = %s,
+                haha_count = %s,
+                hug_count = %s,
+                angry_count = %s,
+                wow_count = %s,
+                sad_count = %s,
+                comment_count = %s,
+                share_count = %s,
+                last_updated_date = %s
+                WHERE post_id = """ + "'" + postObj['post_id'] + "'"
+      val = (postObj['like_count'],
+             postObj['heart_count'],
+             postObj['haha_count'],
+             postObj['hug_count'],
+             postObj['angry_count'],
+             postObj['wow_count'],
+             postObj['sad_count'],
+             postObj['comment_count'],
+             postObj['share_count'],
+             postObj['last_updated_date'])
+      self.dbcursor.execute(sql, val)
+      self.db.commit()
+    else :
+      # Insert Post
+      sql = """INSERT INTO facebook_posts(
+                user_id,
+                post_id,
+                typename,
+                like_count,
+                heart_count,
+                haha_count,
+                hug_count,
+                angry_count,
+                wow_count,
+                sad_count,
+                comment_count,
+                share_count,
+                caption,
+                link,
+                post_created_date,
+                last_updated_date)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+      val = (postObj['user_id'],
+             postObj['post_id'],
+             postObj['typename'],
+             postObj['like_count'],
+             postObj['heart_count'],
+             postObj['haha_count'],
+             postObj['hug_count'],
+             postObj['angry_count'],
+             postObj['wow_count'],
+             postObj['sad_count'],
+             postObj['comment_count'],
+             postObj['share_count'],
+             postObj['caption'],
+             postObj['link'],
+             postObj['post_created_date'],
+             postObj['last_updated_date'])
+      self.dbcursor.execute(sql, val)
+      self.db.commit()
